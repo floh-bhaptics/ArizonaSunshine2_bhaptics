6981640af20f8197d4b6a6b66e2a69fead292382
floh-bhaptics <floh@fahrenbergers.de>
2023-12-06 06:48:06 +0100

    Add project files.

---
+++ ArizonaSunshine2_bhaptics.cs
@@ -0,0 +1,99 @@
+﻿using MelonLoader;
+using MyBhapticsTactsuit;
+using UnityEngine;
+using HarmonyLib;
+using Il2CppVertigo.AZS2.Client;
+using static MelonLoader.MelonLogger;
+
+[assembly: MelonInfo(typeof(ArizonaSunshine2_bhaptics.ArizonaSunshine2_bhaptics), "ArizonaSunshine2_bhaptics", "1.0.0", "Astien & Florian Fahrenberger")]
+[assembly: MelonGame("Vertigo Games", "ArizonaSunshine2")]
+
+
+namespace ArizonaSunshine2_bhaptics
+{
+    public class ArizonaSunshine2_bhaptics : MelonMod
+    {
+        public static TactsuitVR tactsuitVr = null!;
+
+        public override void OnInitializeMelon()
+        {
+            tactsuitVr = new TactsuitVR();
+            tactsuitVr.PlaybackHaptics("HeartBeat");
+        }
+
+        private static KeyValuePair<float, float> getAngleAndShift(Transform player, Vector3 hit, Quaternion playerRotation)
+        {
+            // bhaptics pattern starts in the front, then rotates to the left. 0° is front, 90° is left, 270° is right.
+            // y is "up", z is "forward" in local coordinates
+            Vector3 patternOrigin = new Vector3(0f, 0f, 1f);
+            Vector3 hitPosition = hit - player.position;
+            Quaternion myPlayerRotation = playerRotation;
+            Vector3 playerDir = myPlayerRotation.eulerAngles;
+            // get rid of the up/down component to analyze xz-rotation
+            Vector3 flattenedHit = new Vector3(hitPosition.x, 0f, hitPosition.z);
+
+            // get angle. .Net < 4.0 does not have a "SignedAngle" function...
+            float hitAngle = Vector3.Angle(flattenedHit, patternOrigin);
+            // check if cross product points up or down, to make signed angle myself
+            Vector3 crossProduct = Vector3.Cross(flattenedHit, patternOrigin);
+            if (crossProduct.y > 0f) { hitAngle *= -1f; }
+            // relative to player direction
+            float myRotation = hitAngle - playerDir.y;
+            // switch directions (bhaptics angles are in mathematically negative direction)
+            myRotation *= -1f;
+            // convert signed angle into [0, 360] rotation
+            if (myRotation < 0f) { myRotation = 360f + myRotation; }
+
+
+            // up/down shift is in y-direction
+            // in Shadow Legend, the torso Transform has y=0 at the neck,
+            // and the torso ends at roughly -0.5 (that's in meters)
+            // so cap the shift to [-0.5, 0]...
+            float hitShift = hitPosition.y;
+            //tactsuitVr.LOG("HitShift: " + hitShift);
+            float upperBound = 0.5f;
+            float lowerBound = -0.5f;
+            if (hitShift > upperBound) { hitShift = 0.5f; }
+            else if (hitShift < lowerBound) { hitShift = -0.5f; }
+            // ...and then spread/shift it to [-0.5, 0.5]
+            else { hitShift = (hitShift - lowerBound) / (upperBound - lowerBound) - 0.5f; }
+
+            //tactsuitVr.LOG("Relative x-z-position: " + relativeHitDir.x.ToString() + " "  + relativeHitDir.z.ToString());
+            //tactsuitVr.LOG("HitAngle: " + hitAngle.ToString());
+            //tactsuitVr.LOG("HitShift: " + hitShift.ToString());
+
+            // No tuple returns available in .NET < 4.0, so this is the easiest quickfix
+            return new KeyValuePair<float, float>(myRotation, hitShift);
+        }
+        /*
+        Vector3 hitPosition = zombie.Position;
+        Transform playerPosition = __instance.Transform;
+        Quaternion playerRotation = __instance.BaseRotation;
+        Quaternion headRotation = __instance.HeadRotation;
+        var angleShift = getAngleAndShift(playerPosition, hitPosition, playerRotation * headRotation);
+                if (zombie.Locomotion.IsCrawling)
+                {
+                    tactsuitVr.PlaybackHaptics("ExplosionFeet");
+                    tactsuitVr.PlayBackHit("Slash", angleShift.Key, -0.5f);
+                    return;
+                }
+        tactsuitVr.PlayBackHit("Slash", angleShift.Key, angleShift.Value);
+        */
+
+
+        [HarmonyPatch(typeof(ProjectileShootStrategyBehaviourData), "PlayShootHapticsForHand", new Type[] { typeof(AZS2Hand) })]
+        public class bhaptics_Recoil
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ProjectileShootStrategyBehaviourData __instance, AZS2Hand hand)
+            {
+                string weapon = "Pistol";
+                if (__instance.shootStrategy.projectilesPerBurst > 1) weapon = "Shotgun";
+                bool isRightHand = (hand.IsRightHand);
+                tactsuitVr.Recoil(weapon, isRightHand);
+            }
+        }
+
+
+    }
+}
---
+++ MyBhapticsTactsuit.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using MelonLoader;
+using Bhaptics.SDK2;
+
+namespace MyBhapticsTactsuit
+{
+    public class TactsuitVR
+    {
+        /* A class that contains the basic functions for the bhaptics Tactsuit, like:
+         * - A Heartbeat function that can be turned on/off
+         * - A function to read in and register all .tact patterns in the bHaptics subfolder
+         * - A logging hook to output to the Melonloader log
+         * - 
+         * */
+        public bool suitDisabled = true;
+        public bool systemInitialized = false;
+        // Event to start and stop the heartbeat thread
+        private static ManualResetEvent HeartBeat_mrse = new ManualResetEvent(false);
+        // dictionary of all feedback patterns found in the bHaptics directory
+        public Dictionary<String, FileInfo> FeedbackMap = new Dictionary<String, FileInfo>();
+
+        public void HeartBeatFunc()
+        {
+            while (true)
+            {
+                // Check if reset event is active
+                HeartBeat_mrse.WaitOne();
+                PlaybackHaptics("HeartBeat");
+                Thread.Sleep(600);
+            }
+        }
+
+        public TactsuitVR()
+        {
+            LOG("Initializing suit");
+            var res = BhapticsSDK2.Initialize("7KyidkGqyLsoVibEPMAr", "vvPo6ikLPfDtZ39CV5Og", "");
+
+            if (res > 0)
+            {
+                LOG("Failed to do bhaptics initialization...");
+            }
+            LOG("Starting HeartBeat thread...");
+            Thread HeartBeatThread = new Thread(HeartBeatFunc);
+            HeartBeatThread.Start();
+            PlaybackHaptics("HeartBeat");
+        }
+
+        public void LOG(string logStr)
+        {
+            MelonLogger.Msg(logStr);
+        }
+
+
+
+        public void PlaybackHaptics(String key, float intensity = 1.0f, float duration = 1.0f, float xzAngle = 0f, float yShift = 0f)
+        {
+            int res;
+            res = BhapticsSDK2.Play(key.ToLower(), intensity, duration, xzAngle, yShift);
+            // LOG("Playing back: " + key);
+            //if (res > 0) LOG("Playback failed: " + key + " " + key.ToLower());
+        }
+
+
+        public void Recoil(string weaponName, bool isRightHand, bool twoHanded = false, float intensity = 1.0f)
+        {
+            // weaponName is a parameter that will go into the vest feedback pattern name
+            // isRightHand is just which side the feedback is on
+            // intensity should usually be between 0 and 1
+
+            // make postfix according to parameter
+            string postfix = "_L";
+            string otherPostfix = "_R";
+            if (isRightHand) { postfix = "_R"; otherPostfix = "_L"; }
+
+            // stitch together pattern names for Arm and Hand recoil
+            string keyHands = "RecoilHands" + postfix;
+            string keyArm = "RecoilArms" + postfix;
+            string keyOtherHand = "RecoilHands" + otherPostfix;
+            string keyOtherArm = "RecoilArms" + otherPostfix;
+            // vest pattern name contains the weapon name. This way, you can quickly switch
+            // between swords, pistols, shotguns, ... by just changing the shoulder feedback
+            // and scaling via the intensity for arms and hands
+            string keyVest = "Recoil" + weaponName + "Vest" + postfix;
+            PlaybackHaptics(keyHands);
+            PlaybackHaptics(keyArm);
+            PlaybackHaptics(keyVest);
+            if (twoHanded)
+            {
+                PlaybackHaptics(keyOtherHand);
+                PlaybackHaptics(keyOtherArm);
+            }
+        }
+
+        public void FootStep(bool isRightFoot)
+        {
+            if (!BhapticsSDK2.IsDeviceConnected(PositionType.FootL)) { return; }
+            string postfix = "_L";
+            if (isRightFoot) { postfix = "_R"; }
+            string key = "FootStep" + postfix;
+            PlaybackHaptics(key);
+        }
+
+        public void PlayBackHit(String key, float xzAngle, float yShift)
+        {
+            // two parameters can be given to the pattern to move it on the vest:
+            // 1. An angle in degrees [0, 360] to turn the pattern to the left
+            // 2. A shift [-0.5, 0.5] in y-direction (up and down) to move it up or down
+            BhapticsSDK2.Play(key.ToLower(), 1f, 1f, xzAngle, yShift);
+        }
+
+        public void StartHeartBeat()
+        {
+            HeartBeat_mrse.Set();
+        }
+
+        public void StopHeartBeat()
+        {
+            HeartBeat_mrse.Reset();
+        }
+
+        public bool IsPlaying(String effect)
+        {
+            return BhapticsSDK2.IsPlaying(effect.ToLower());
+        }
+
+        public void StopHapticFeedback(String effect)
+        {
+            BhapticsSDK2.Stop(effect.ToLower());
+        }
+
+        public void StopAllHapticFeedback()
+        {
+            StopThreads();
+            BhapticsSDK2.StopAll();
+        }
+
+        public void StopThreads()
+        {
+            // Yes, looks silly here, but if you have several threads like this, this is
+            // very useful when the player dies or starts a new level
+            StopHeartBeat();
+        }
+
+
+    }
+}
